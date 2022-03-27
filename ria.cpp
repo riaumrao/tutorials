d4f0e6fe29607d887018b1b8ae3d9227a28cb4dd
riaumrao <63940742+riaumrao@users.noreply.github.com>
2022-03-27 12:35:29 +0530

    first file

---
+++ ria.cpp
@@ -0,0 +1,14 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+int fn (int m,int n)
+{
+    return m+n;
+}
+int main ()
+{
+    int y= fn(3,2);
+    // Write C++ code here
+std::cout << "addition result" <<y;
+    return 0;
+}
+
